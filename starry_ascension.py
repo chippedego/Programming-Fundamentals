4c17de9354de67094ddae2d76fe725217d84a945
chippedego <98s5tz6mtj@privaterelay.appleid.com>
2026-09-11 22:13:52 +0800

    Loops and Repetition
    this code is mainly used to practice loops and repetition.
    it teaches you how to make a program repeat something automatically instead of writing it yourself five times.
    
    without a loop, you'd have to do:
    #print("*")
    #print("**")
    #print("***")
    #print("****")
    #print("*****")

--- starry_ascension.py
+++ starry_ascension.py
@@ -1,12 +1,2 @@
 for i in range (1,6):
   print("*" * i)
-
-#this code is mainly used to practice loops and repetition
-#it teaches you how to make a program repeat something automatically instead of writing it yourself five times
-
-#without a loop, you'd have to do:
-#print("*")
-#print("**")
-#print("***")
-#print("****")
-#print("*****")
